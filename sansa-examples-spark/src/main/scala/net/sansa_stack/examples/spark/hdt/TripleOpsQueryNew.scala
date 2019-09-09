e8753ad2af05d3fbf170ea5f7913d9f1d62454ad
ibhadavid <david.o.ibhaluobe@gmail.com>
2019-09-10 00:42:08 +0530

    Added the Configuration
    TripleOpsQueryNew <tripleFilePath> <resultDir> <queryDir>

--- sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/hdt/TripleOpsQueryNew.scala
+++ sansa-examples-spark/src/main/scala/net/sansa_stack/examples/spark/hdt/TripleOpsQueryNew.scala
@@ -11,6 +11,9 @@ import org.apache.spark.sql.SparkSession
 import net.sansa_stack.rdf.spark.io._
 import org.apache.spark.rdd.RDD
 import net.sansa_stack.query.spark.query._
+import java.io.File
+
+import scala.io.Source
 
 class TripleOpsQueryNew() {
 
@@ -169,51 +172,86 @@ object TripleOpsQueryNew {
     pw.append(s"${dfCount} | ${diff} |")
 
     start=System.currentTimeMillis()
-    val count=rdfTriple.sparql(query).count()
-    pw.append(s"${count} |  ${(System.currentTimeMillis()-start)/1000.0} ")
+   // val count=rdfTriple.sparql(query).count()
+    //pw.append(s"${count} |  ${(System.currentTimeMillis()-start)/1000.0} ")
     pw.append("\n")
     pw.close()
   }
+
+
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("TripleOps").master("local[*]").getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
-    val tripleFile="/home/david/SANSA-Examples/sansa-examples-spark/src/main/resources/small/bsbm/sample.nt"
-    val resultDir="/home/david/SANSA-Examples/sansa-examples-spark/src/main/resources/small/bsbm/output"
+
+    if(args.length<3){
+      println( " Invalid Arguments. ")
+      println("Usage:")
+      println("TripleOpsQueryNew <tripleFilePath> <resultDir> <queryDir>")
+    }
+    //val tripleFile="/Users/jignesh/Desktop/Fiverr/David/SANSA-Examples/sansa-examples-spark/src/main/resources/small/lubm/sample.nt"
+    val tripleFile=args(0)
+    println(s"Triple File: ${tripleFile}")
+    //val resultDir="/Users/jignesh/Desktop/Fiverr/David/SANSA-Examples/sansa-examples-spark/src/main/resources/output/lubm"
+
+    val resultDir=args(1)
+    println(s"Result Dir: ${resultDir}")
+
+    val queryDir=args(2)
+    println(s"Query Dir: ${queryDir}")
+
+
     val lang = Lang.NTRIPLES
     val rdfTriple = spark.rdf(lang)(tripleFile);
     val hdtDF = TripleOps.getHDT(rdfTriple)
 
-    var query=""
-    query="SELECT ?S ?O ?P WHERE { ?S <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productPropertyTextual4> ?P .  }"
+    val d = new File(queryDir)
+
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList.map(f=>{
+        print("Reading ${f.getAbsolutePath}")
+        println(s"Executing Query: ${Source.fromFile(f.getAbsolutePath).mkString} ")
+        execute(spark,rdfTriple,Source.fromFile(f.getAbsolutePath).mkString,resultDir)
+      })
+    } else {
+      println("Please provide the Query directory.")
+      System.exit(1)
+    }
 
 
-    query="SELECT ?S ?O ?P  WHERE { ?S ?P ?O }"
-    execute(spark,rdfTriple,query,resultDir)
+    /*
+     var query=""
 
-     query="SELECT (COUNT(*) AS ?A)  WHERE { ?S ?P ?O }"
+     query="SELECT ?S ?O ?P  WHERE { ?S ?P ?O }"
      execute(spark,rdfTriple,query,resultDir)
 
-    query="SELECT DISTINCT ?S ?O ?P WHERE { ?S ?P ?O }"
-    execute(spark,rdfTriple,query,resultDir)
+     query="SELECT ?S ?O ?P WHERE { ?S <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?O .  }"
+     execute(spark,rdfTriple,query,resultDir)
 
-    query="PREFIX foo: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/> PREFIX hoo: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/> SELECT ?S ?O ?P WHERE { foo:ProductType2 ?P ?O .  } limit 10"
-    execute(spark,rdfTriple,query,resultDir)
+      query="SELECT (COUNT(*) AS ?A)  WHERE { ?S ?P ?O }"
+      execute(spark,rdfTriple,query,resultDir)
+
+     query="SELECT DISTINCT ?S ?O ?P WHERE { ?S ?P ?O }"
+     execute(spark,rdfTriple,query,resultDir)
 
 
-    query="prefix test: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/dataFromProducer2/> SELECT ?S ?O ?P WHERE { test:Product92 ?P ?O . ?S <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productPropertyTextual1> ?O . }"
-    execute(spark,rdfTriple,query,resultDir)
+     query="PREFIX foo: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/> PREFIX hoo: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/> SELECT ?S ?O ?P WHERE { foo:ProductType2 ?P ?O .  }"
+     execute(spark,rdfTriple,query,resultDir)
 
-    query="SELECT ?S ?O ?P WHERE { <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/dataFromProducer2/Product92> ?P ?O . ?S <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productPropertyTextual1> ?O . }"
-    execute(spark,rdfTriple,query,resultDir)
 
+     query="prefix test: <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/> SELECT ?S ?O ?P WHERE { test:ProductFeature92 ?P ?O . ?S <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/StandardizationInstitution1> ?O . }"
+     execute(spark,rdfTriple,query,resultDir)
+
+    query="SELECT ?S ?O ?P WHERE { ?S ?P ?O . FILTER ( STRLEN(?S) >= 40 ) . }"
+     execute(spark,rdfTriple,query,resultDir)
 
     query="""
          SELECT ?s ?p WHERE {
-         ?s ?p <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/dataFromProducer2/Product92> .
-         OPTIONAL { ?s <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/productPropertyTextual1> ?o }
+         ?s ?p <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> .
+         OPTIONAL { ?s <http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/instances/StandardizationInstitution1> ?o }
         }
       """.stripMargin
     execute(spark,rdfTriple,query,resultDir)
+  */
 }
 
 }
